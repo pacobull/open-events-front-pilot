e9ed1bf279306b212d276c7e9013bdafa13adc88
francisco.toro.mateo <francisco.toro.mateo@accenture.com>
2019-12-15 10:07:49 +0100

    fixing lint

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -6,7 +6,8 @@ const EVENTS: Event[] = [
     id: '0',
     title: 'Introduction to JS - basic',
     location: 'Barcelona',
-    date: new Date("2020-03-16"),
+    date: new Date('2020-03-16'),
+    // tslint:disable-next-line: max-line-length
     description: 'Nulla aliqua duis adipisicing do amet et ullamco commodo id laborum nulla ipsum culpa. Lorem ipsum commodo quis amet consequat nostrud esse est deserunt. Laboris incididunt esse amet sunt tempor pariatur nisi irure nulla veniam id quis elit. Velit officia quis veniam aliqua. Cupidatat velit enim officia dolor ea veniam proident culpa ea duis labore nostrud. Occaecat in velit esse et. Duis anim ad elit ipsum occaecat Lorem veniam labore consequat laboris non.',
     addedBy: 'user01'
 
@@ -15,7 +16,8 @@ const EVENTS: Event[] = [
     id: '1',
     title: 'Introduction to Angular',
     location: 'London',
-    date: new Date("2019-10-28"),
+    date: new Date('2019-10-28'),
+    // tslint:disable-next-line: max-line-length
     description: 'Nulla aliqua duis adipisicing do amet et ullamco commodo id laborum nulla ipsum culpa. Lorem ipsum commodo quis amet consequat nostrud esse est deserunt. Laboris incididunt esse amet sunt tempor pariatur nisi irure nulla veniam id quis elit. Velit officia quis veniam aliqua. Cupidatat velit enim officia dolor ea veniam proident culpa ea duis labore nostrud. Occaecat in velit esse et. Duis anim ad elit ipsum occaecat Lorem veniam labore consequat laboris non.',
     addedBy: 'user01'
   },
@@ -23,7 +25,8 @@ const EVENTS: Event[] = [
     id: '2',
     title: 'Introduction to RXJS',
     location: 'London',
-    date: new Date("2019-10-02"),
+    date: new Date('2019-10-02'),
+    // tslint:disable-next-line: max-line-length
     description: 'Nulla aliqua duis adipisicing do amet et ullamco commodo id laborum nulla ipsum culpa. Lorem ipsum commodo quis amet consequat nostrud esse est deserunt. Laboris incididunt esse amet sunt tempor pariatur nisi irure nulla veniam id quis elit. Velit officia quis veniam aliqua. Cupidatat velit enim officia dolor ea veniam proident culpa ea duis labore nostrud. Occaecat in velit esse et. Duis anim ad elit ipsum occaecat Lorem veniam labore consequat laboris non.',
     addedBy: 'user01'
   },
@@ -31,7 +34,8 @@ const EVENTS: Event[] = [
     id: '3',
     title: 'AWS',
     location: 'Berlin',
-    date: new Date("2019-11-21"),
+    date: new Date('2019-11-21'),
+    // tslint:disable-next-line: max-line-length
     description: 'Nulla aliqua duis adipisicing do amet et ullamco commodo id laborum nulla ipsum culpa. Lorem ipsum commodo quis amet consequat nostrud esse est deserunt. Laboris incididunt esse amet sunt tempor pariatur nisi irure nulla veniam id quis elit. Velit officia quis veniam aliqua. Cupidatat velit enim officia dolor ea veniam proident culpa ea duis labore nostrud. Occaecat in velit esse et. Duis anim ad elit ipsum occaecat Lorem veniam labore consequat laboris non.',
     addedBy: 'user01'
   },
@@ -39,11 +43,12 @@ const EVENTS: Event[] = [
     id: '4',
     title: 'Angular NgRx - introduction',
     location: 'Madrid',
-    date: new Date("2019-12-05"),
+    date: new Date('2019-12-05'),
+    // tslint:disable-next-line: max-line-length
     description: 'Nulla aliqua duis adipisicing do amet et ullamco commodo id laborum nulla ipsum culpa. Lorem ipsum commodo quis amet consequat nostrud esse est deserunt. Laboris incididunt esse amet sunt tempor pariatur nisi irure nulla veniam id quis elit. Velit officia quis veniam aliqua. Cupidatat velit enim officia dolor ea veniam proident culpa ea duis labore nostrud. Occaecat in velit esse et. Duis anim ad elit ipsum occaecat Lorem veniam labore consequat laboris non.',
     addedBy: 'user01'
   }
-]
+];
 
 @Component({
   selector: 'oevents-event-list',
