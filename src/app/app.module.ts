7821b5a309c989b0b2c0ff77326ce84cd03473d6
francisco.toro.mateo <francisco.toro.mateo@accenture.com>
2019-12-15 10:59:39 +0100

    Obteniendo los eventos mediante HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // Modules
+import { CoreModule } from './core/core.module'; // <--- NEW
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 import { EventsModule } from './events/events.module';
 import { LoginModule } from './login/login.module';
 import { ProfileModule } from './profile/profile.module';
@@ -12,7 +14,6 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { AppRoutingModule } from './app-routing.module';
     PageNotFoundComponent
   ],
   imports: [
+    CoreModule,
     BrowserModule,
     AppRoutingModule,
     SharedModule,
--- src/app/core/event.service.ts
+++ src/app/core/event.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +19,26 @@ export class EventService {
       'Content-Type': 'application/json'
     });
 
-    return this.http.get('assets/events.json', { headers });
+    return this.http.get(environment.apiURL + 'events', { headers }).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
+  }
+
+  // Error handling
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues about what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
   }
 }
